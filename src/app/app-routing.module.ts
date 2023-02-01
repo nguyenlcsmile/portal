545cd95ad1627243ba3a156ff9ee0cfa122ad34a
user-at-704207050840 <caolenguyen@deltavn.vn>
2023-02-01 16:49:34 +0700

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MonitorPageComponent } from './components/monitor-page/monitor-page.component';
+import { CustomerPageComponent } from './components/customer-page/customer-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'home-page', pathMatch: 'full' },
+    { path: 'home-page', component: HomePageComponent},
+    { path: 'monitor-page', component: MonitorPageComponent},
+    { path: 'customer-page', component: CustomerPageComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { CustomerPageComponent } from './components/customer-page/customer-page.component';
+import { MonitorPageComponent } from './components/monitor-page/monitor-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SidebarComponent,
+        HeaderComponent,
+        CustomerPageComponent,
+        MonitorPageComponent,
+        HomePageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
